ce1de2964d7106a921102824507b1e4e0afb6e30
Santiago <saantii.28@gmail.com>
2016-07-08 19:55:53 -0300

    Add delete method and change json file library

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var app = require('express')();
 var bodyParser = require('body-parser');
-var jsonfile = require('jsonfile')
+var db = require('diskdb');
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
@@ -10,31 +10,43 @@ app.use(function(req, res, next) {
   next();
 });
 
-var file = './data-dev.json';
+db.connect('./db', ['products']);
 
 app.get('/products', function (req, res) {
-  res.json(getProducts());
+  var products = getProducts();
+  res.status(200).json(products);
 });
 
 app.post('/products', function (req, res) {
   var product_name = req.body.name;
   console.log(product_name);
+
   var result = createProduct(product_name);
+
   result ?
-    res.status(201).send("Agregado el nuevo producto") :
+    res.status(201).send(result) :
     res.status(400).send("Nombre invalido");
 });
 
+app.delete("/products/:id", function(req, res) {
+  var id = req.params.id;
+
+  var result = deleteProduct(id);
+
+  res.status(200).send(result);
+});
+
 var getProducts = function() {
-  return jsonfile.readFileSync(file);
+  return db.products.find();
 }
 
 var createProduct = function(product_name) {
-  var db_actual = jsonfile.readFileSync(file);
   if (product_name === undefined || product_name === "") return false;
-  var db_new = db_actual.concat([product_name]);
-  jsonfile.writeFileSync(file, db_new, {spaces: 2});
-  return true;
+  return db.products.save({ name: product_name });
+}
+
+var deleteProduct = function(id) {
+  return db.products.remove({ _id: id }, false);
 }
 
 app.listen(3001, function () {
